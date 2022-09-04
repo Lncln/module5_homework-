8319d8fdebd32af0dd5e4830b6d520565ab284b6
L <roma200998@mail.ru>
2022-09-04 09:42:46 +0500

    ?

--- Задание 8.js
+++ Задание 8.js
@@ -10,3 +10,10 @@ for (let heroParameters of Hero.keys()) {
     let parametersValues = Hero.get(heroParameters)
     console.log(`Ключ - ${heroParameters}, значение - ${parametersValues}`);
 }
+
+// Почему нельзя так?
+// let Hero = {Level: 99, Class: 'Wizard', PrimaryAttribute: 'Int'};
+// for (let heroParameters in Hero) {
+//     let parametersValues = Hero[heroParameters]
+//     console.log(`Ключ - ${heroParameters}, значение - ${parametersValues}`);
+// }
